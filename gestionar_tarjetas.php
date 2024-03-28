45b64e0be5067ebe48f9891d0f2aa83edd516f9d
zeluob97 <162311160+zeluob97@users.noreply.github.com>
2024-03-28 18:06:10 +0100

    gestionar tarjetas creado para user

---
+++ gestionar_tarjetas.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>G4A</title>
+        <link rel="stylesheet" href="estilos.css">
+        <div id="header">
+        <h1>GAMES4ALL</h1>
+        <h4>¡Consigue tu juego preferido al mejor precio!</h4>
+    </div>
+    </head>
+
+    
+    <body style="background-color: #4CC5B0; text-align: center; color: #000000;">
+        <div style="float: left; width: 20%; height: 450px;margin-top: -60px; background-color: #173E59; color: #ffffff;font-size: 25px;">
+           <h4>TUS TARJETAS</h4>
+	       <h5>PONER COMO EN GESTIONAR USUARIOS, UNA TABLA LISTANDO Y QUE SE PUEDA EDITAR Y ELIMINAR</h5>
+           
+           
+
+           
+
+        </div>
+    </body>
+</html>
