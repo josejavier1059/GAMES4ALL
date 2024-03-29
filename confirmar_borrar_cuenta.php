f0eafe0b6c1bc3e113a0bd1f1f97993d0aa86ce0
zeluob97 <162311160+zeluob97@users.noreply.github.com>
2024-03-30 00:43:38 +0100

    user borra cuenta propia hecho

--- confirmar_borrar_cuenta.php
+++ confirmar_borrar_cuenta.php
@@ -1,3 +1,10 @@
+<?php
+
+    $conexion = mysqli_connect("localhost", "root", "", "games4all")or die("Fallo al hacer la consulta.");
+    $alias = $_COOKIE['alias'];
+
+    ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,14 +17,24 @@
     </div>
     </head>
 
-    <?php
-    $alias = $_COOKIE['alias'];
-    ?>
+
     <body style="background-color: #4CC5B0; text-align: center; color: #000000;">
         <div style="float: left; width: 20%; height: 450px;margin-top: -60px; background-color: #173E59; color: #ffffff;font-size: 25px;">
            <h4>BORRAR CUENTA</h4>
-	                  
-           
+	      
+            <?php  
+                if($conexion){    
+                    if(mysqli_query($conexion, "DELETE FROM usuario WHERE alias = '$alias'")){
+                        echo '¡Usuario eliminado con éxito!';
+                        header("refresh:3;url=index.php?logout=true");          
+                    }
+                    else{
+                        echo 'Se ha producido un error, no se ha podido eliminar su cuenta.';
+                        header("refresh:3;url=gestionar_perfil.php");  
+                    }
+                }    
+            ?>          
+            
 
         </div>
     </body>
